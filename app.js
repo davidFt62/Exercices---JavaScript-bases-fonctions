2a7939601af9b21f561970b7e55e286e096b2d10
Enki <da.foucart@gmail.com>
2022-12-22 16:01:46 +0100

    évolution exo

--- app.js
+++ app.js
@@ -66,27 +66,60 @@ if (parseInt(age) && typeof (parseInt(age)) === 'number' && (age > 0 && age < 18
 }
 
 
+console.log("")
+
+
 //************************************************************//
 
 
+//Création et parcours d'un tableau
+
+//1. Tableau appelé `couleurs` qui contient les éléments suivants : "rouge", "vert", "bleu", "jaune", "orange".
+//2. Boucle for pour parcourir le tableau couleurs
+//3. Console.log() affiche chaque élément dans la console en utilisant la fonction .
+
+let couleurs = ['rouge', 'vert', 'bleu', 'jaune', 'orange']//1
+for (let i = 0; i < couleurs.length; i++) //2
+{
+    console.log(couleurs[i])//3
+}
+
+console.log("");
 
 
+//************************************************************//
 
 
+//Utilisation de Array.push() et Array.pop()
+
+//1. Tableau appelé `numbers` qui contient les nombres suivants : 1, 2, 3, 4, 5.
+//2. push() pour ajouter le nombre 6 au tableau numbers.
+//3. pop() pour retirer le dernier élément du tableau numbers.
+//4. Affiche le contenu du tableau numbers dans la console en utilisant la fonction console.log().
 
 
+ numbers = [1, 2, 3, 4, 5];  //1
+ push6 = numbers.push(6);//2  
+ console.log(numbers);//4
+
+console.log("");
+
+ pop6 = numbers.pop(6);//3  
+ console.log(numbers);//4
+
+console.log("");
 
 
+//************************************************************//
 
 
+//Fonctions
 
+//1. Écrivez une fonction qui prend en argument un entier et renvoie `true` si l'entier est pair, `false` sinon. Vous pouvez utiliser l'opérateur modulo `%` pour vérifier si un nombre est divisible par deux.
 
+//2. Écrivez une fonction qui prend en argument un tableau d'entiers et renvoie la somme de tous les éléments du tableau. Vous pouvez utiliser une boucle `for` pour parcourir le tableau et ajouter chaque élément à une variable qui tiendra la somme.
 
-
-
-
-
-
+//3. Écrivez une fonction qui prend en argument une chaîne de caractères et renvoie la chaîne de caractères inversée. Par exemple, si la chaîne passée en argument est "bonjour", la fonction doit renvoyer "ruojnob". Vous pouvez utiliser la méthode `split()` pour séparer la chaîne en un tableau de caractères, la méthode `reverse()` pour inverser l'ordre des éléments du tableau, et la méthode `join()` pour reconstruire la chaîne de caractères à partir du tableau inversé.
 
 
 
